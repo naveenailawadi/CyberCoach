13454a9d0c3df70c6847dcabcc25cfa46c9d0db4
Naveen Ailawadi <naveen.ailawadi91@gmail.com>
2021-09-01 08:01:07 -0400

    fixed countdown

--- core/tools.py
+++ core/tools.py
@@ -5,7 +5,7 @@ import time
 def countdown(t):
     while t >= 0:
         mins, secs = divmod(t, 60)
-        timer = '{:02d}:{:02d}'.format(mins, secs)
+        timer = '{:02d}:{:02d}'.format(int(mins), int(secs))
         print(timer, end="\r")
         time.sleep(1)
         t -= 1
